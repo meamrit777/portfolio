b6c3ab98c054425c859184cfd8c20b4e2f2cabc8
Amrit Shrestha <me.amrit777@gmail.com>
2023-01-02 22:23:02 +0545

    contact and footer added and the project is ready

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
-    return <section id="footer"> Footer</section>;
-    
-}
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        SHRESTHA
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonial">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https:facebook.com/meamrit777">
+          <FaFacebookF />
+        </a>
+        <a href="https:instagram.com/cr_amrit">
+          <FiInstagram />
+        </a>
+        <a href="https:twitter.com/meamrit777">
+          <IoLogoTwitter />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Amrit Shrestha. All rights reserved. </small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
